27c648b5389d70d0c7a0fc33d968ecd1ce389778
Nora <nob@pdx.edu>
2023-10-25 19:11:20 -0700

    basic index.html get request working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-/*
-Base code from https://actix.rs/docs/getting-started
-*/
+// Base code from https://actix.rs/docs/getting-started
+use actix_files::NamedFile;
+use actix_web::{ web, App, HttpServer, Result, Error};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+//#[get("/")]
+//https://actix.rs/docs/static-files
+async fn index() -> Result<NamedFile, Error> {
+    Ok(NamedFile::open("static/index.html")?)
 }
 
+/*
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
@@ -18,13 +18,15 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+*/
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/{index.html:}", web::get().to(index))
+            //.service(echo)
+            //.route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
