59b324973df6b853b9a24a347b5382abe66d684c
Nora <nob@pdx.edu>
2023-10-28 13:13:52 -0700

    fixed templates for use with tera, added tera function to render templates

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 // Base code from https://actix.rs/docs/getting-started
 use actix_files::NamedFile;
 use actix_web::{ web, App, HttpServer, Result, Error};
+use tera::Tera;
+use lazy_static::lazy_static;
 
-//#[get("/")]
 //https://actix.rs/docs/static-files
 async fn index() -> Result<NamedFile, Error> {
     Ok(NamedFile::open("static/index.html")?)
 }
 
-/*
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+//https://keats.github.io/tera/docs/
+fn html_template_init() {
+    lazy_static! {
+        pub static ref TEMPLATES: Tera = {
+            let mut tera = match Tera::new("/templates/**/*") {
+                Ok(t) => t,
+                Err(e) => {
+                    println!("Parsing error(s): {}", e);
+                    ::std::process::exit(1);
+                }
+            };
+            tera.autoescape_on(vec![".html", ".sql"]);
+            //tera.register_filter("do_nothing", do_nothing_filter);
+            tera
+        };
+    }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-*/
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    html_template_init();
+
     HttpServer::new(|| {
         App::new()
             .route("/{index.html:}", web::get().to(index))
